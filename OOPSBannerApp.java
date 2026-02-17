8c3edfe209f2ccc133c7839e57acfd60691a05a7
mahadev-srm <mb4756@srmist.edu.in>
2026-02-17 09:23:10 +0530

    UC2: Implemented 7-line ASCII banner for OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,23 @@
+/**
+ * OOPSBannerApp
+ *
+ * Use Case 2:
+ * Display "OOPS" as a large banner using '*' characters.
+ *
+ * @author Mahadev
+ * @version 2.0
+ */
+
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****    *****   ***** ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *****    ***** ");
+        System.out.println("*     *  *     *  *        *     ");
+        System.out.println("*     *  *     *  *        *     ");
+        System.out.println(" *****    *****   *        ***** ");
     }
 }
